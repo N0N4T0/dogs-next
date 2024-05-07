fa8de66f56cd2d908c2229886be3f201448298fc
Lucio <luciononato@hotmail.com>
2024-05-06 23:30:41 -0300

    fix: fetch

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,11 +1,10 @@
-import dynamic from 'next/dynamic';
-const Width = dynamic(() => import('@/components/width'), { ssr: false });
+import ClientFetch from '@/components/client-fetch';
 
 export default function SobrePage() {
   return (
     <main>
       <h1>Sobre</h1>
-      <Width />
+      <ClientFetch />
     </main>
   );
 }
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -9,9 +9,6 @@ export default function Menu() {
       <li>
         <Link href="/sobre">Sobre</Link>
       </li>
-      <li>
-        <Link href="/imc">IMC</Link>
-      </li>
     </ul>
   );
 }
