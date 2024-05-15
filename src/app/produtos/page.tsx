6ca0451befc47152d0b7b5afdd063392b80a4623
Lucio <luciononato@hotmail.com>
2024-05-14 22:58:26 -0300

    fix add error

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,30 +1,11 @@
-export type Produto = {
-  id?: string;
-  nome: string;
-  preco: number;
-  descricao: string;
-  estoque: number;
-  importado: 0 | 1;
-};
+import ProdutosLista from '@/components/produtos-lista';
 
 export default async function ProdutosPage() {
-  const response = await fetch('https://api.origamid.online/produtos', {
-    next: {
-      revalidate: 5,
-    },
-  });
-  const produtos = (await response.json()) as Produto[];
-
   return (
     <main>
       <h1>Produtos</h1>
-      <ul>
-        {produtos.map((produto) => (
-          <li key={produto.id}>
-            {produto.nome}: R$ {produto.preco}
-          </li>
-        ))}
-      </ul>
+      <p>Essa aqui é a lista de produtos</p>
+      <ProdutosLista />
     </main>
   );
 }
---
+++ src/components/produtos-lista.tsx
@@ -0,0 +1,32 @@
+export type Produto = {
+  id?: string;
+  nome: string;
+  preco: number;
+  descricao: string;
+  estoque: number;
+  importado: 0 | 1;
+};
+
+export default async function ProdutosLista() {
+  let produtos: Produto[] = [];
+  try {
+    const response = await fetch('https://api.origamid.online/produtoss', {
+      next: {
+        revalidate: 5,
+      },
+    });
+    if (!response.ok) throw new Error('Erro ao carregar os produtos.');
+    produtos = (await response.json()) as Produto[];
+  } catch (err) {
+    return <p>Ocorreu um erro nessa lista de produtos.</p>;
+  }
+  return (
+    <ul>
+      {produtos.map((produto) => (
+        <li key={produto.id}>
+          {produto.nome}: R$ {produto.preco}
+        </li>
+      ))}
+    </ul>
+  );
+}
